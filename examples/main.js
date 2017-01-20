7788f8152ebcc2cfb46ae529fd77574b0f721247
Denis <carriere.denis@gmail.com>
2017-01-20 15:49:50 -0500

    Add examples Webpack & Browserify

---
+++ examples/main.js
@@ -0,0 +1,27 @@
+const wmts = require('../')
+
+console.log(`
+wmts.getCapabilities({
+  uri: 'http://localhost:5000/WMTS',
+  title: 'Tile Service XYZ',
+  identifier: 'service-123',
+  abstract: '© OSM data',
+  keyword: ['world', 'imagery', 'wmts'],
+  format: 'png',
+  minzoom: 10,
+  maxzoom: 18,
+  bbox: [-180, -85, 180, 85]
+})
+`)
+const xml = wmts.getCapabilities({
+  uri: 'http://localhost:5000/WMTS',
+  title: 'Tile Service XYZ',
+  identifier: 'service-123',
+  abstract: '© OSM data',
+  keyword: ['world', 'imagery', 'wmts'],
+  format: 'png',
+  minzoom: 10,
+  maxzoom: 18,
+  bbox: [-180, -85, 180, 85]
+})
+console.log(xml)

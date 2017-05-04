cc25864e9122d3b3ff493299aa8a33053177411a
Denis <carriere.denis@gmail.com>
2017-05-04 17:53:11 -0400

    Update typescript definition

--- index.d.ts
+++ index.d.ts
@@ -1,27 +1,23 @@
 export type BBox = [number, number, number, number] // [west, south, east, north]
 export type Format = 'png' | 'jpeg' | 'jpg'
-
-interface Options extends ServiceIdentificationOptions, LayerOptions {
-  spaces?: number
-  minzoom?: number
-  maxzoom?: number
-}
-
-interface LayerOptions {
+export interface Options extends ServiceIdentification, Layer { spaces?: number }
+export interface Metadata extends ServiceIdentification, Layer {}
+export interface Layer {
   title: string
   url: string
   format: Format
   abstract?: string
   identifier?: string
   bbox?: BBox
+  minzoom?: number
+  maxzoom?: number
 }
-
-interface ServiceIdentificationOptions {
+export interface ServiceIdentification {
   title: string
   abstract?: string
   keywords?: string[]
   accessConstraints?: string
   fees?: string
 }
-
 export function getCapabilities (options: Options): string
+export function parseCapabilities (xml: string): Metadata

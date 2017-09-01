804dafee3e94167ed8cb1c3fc241b014396833a1
Denis <carriere.denis@gmail.com>
2017-09-01 10:32:17 -0400

    Finish Layer

--- src/wmts.js
+++ src/wmts.js
@@ -2,6 +2,10 @@ const xpath = require('xpath')
 const DOMParser = require('xmldom').DOMParser
 const utils = require('./utils')
 const clean = utils.clean
+const select = xpath.useNamespaces({
+  'ows': 'http://www.opengis.net/ows/1.1',
+  'xlink': 'http://www.w3.org/1999/xlink'
+})
 
 /**
  * WMTS Metadata
@@ -9,8 +13,7 @@ const clean = utils.clean
  * @typedef {Object} Metadata
  * @property {Layer} layer
  * @property {Service} service
- * @property {string} format
- * @property {string} abstract
+ * @property {URL} url
  */
 
 /**
@@ -25,6 +28,7 @@ const clean = utils.clean
  * @property {number} minzoom
  * @property {number} maxzoom
  * @property {[number, number, number, number]} bbox
+ * @property {string[]} tileMatrixSets
  */
 
 /**
@@ -36,8 +40,18 @@ const clean = utils.clean
  * @property {string} title
  */
 
+ /**
+ * WMTS Metadata.Layer.URL
+ *
+ * @typedef {Object} URL
+ * @property {string} host
+ * @property {string} slippy
+ * @property {string} getCapabilities
+ * @property {string} getTile
+ */
+
 function selectZooms (node) {
-  const identifiers = xpath.select("//*[name()='TileMatrixSet']/*[name()='TileMatrix']/*[name()='ows:Identifier']", node)
+  const identifiers = select('//TileMatrixSet/TileMatrix/ows:Identifier', node)
   let minzoom
   let maxzoom
   for (const identifier of identifiers) {
@@ -52,8 +66,8 @@ function selectZooms (node) {
 }
 
 function selectBBox (node) {
-  var southwest = xpath.select("string(//*[name()='ows:WGS84BoundingBox']//*[name()='ows:LowerCorner'])", node)
-  var northeast = xpath.select("string(//*[name()='ows:WGS84BoundingBox']//*[name()='ows:UpperCorner'])", node)
+  var southwest = select('string(//ows:WGS84BoundingBox//ows:LowerCorner)', node, true)
+  var northeast = select('string(//ows:WGS84BoundingBox//ows:UpperCorner)', node, true)
   if (southwest && northeast) {
     southwest = southwest.split(' ')
     northeast = northeast.split(' ')
@@ -66,12 +80,27 @@ function selectBBox (node) {
  *
  * @param {Document} doc
  * @returns {Layer} layer
+ * @example
+ * <Layer>
+ *   <ows:Title>Satellite Streets</ows:Title>
+ *   <ows:Identifier>ciy23jhla008n2soz34kg2p4u</ows:Identifier>
+ *   <ows:Abstract>© OSM, © DigitalGlobe</ows:Abstract>
+ *   <ows:WGS84BoundingBox crs="urn:ogc:def:crs:OGC:2:84">
+ *     <ows:LowerCorner>-180 -85.051129</ows:LowerCorner>
+ *     <ows:UpperCorner>179.976804 85.051129</ows:UpperCorner>
+ *   </ows:WGS84BoundingBox>
+ *   <Style isDefault="true"><ows:Identifier>default</ows:Identifier></Style>
+ *   <Format>image/jpeg</Format>
+ *   <TileMatrixSetLink><TileMatrixSet>GoogleMapsCompatible</TileMatrixSet></TileMatrixSetLink>
+ *   <ResourceURL format="image/jpeg" resourceType="tile" template="https://api.mapbox.com/styles/v1/addxy/ciy23jhla008n2soz34kg2p4u/tiles/{TileMatrix}/{TileCol}/{TileRow}?access_token=pk.eyJ1IjoiYWRkeHkiLCJhIjoiY2lsdmt5NjZwMDFsdXZka3NzaGVrZDZtdCJ9.ZUE-LebQgHaBduVwL68IoQ"/>
+ * </Layer>
  */
 function layer (doc) {
-  const title = xpath.select("string(//*[name()='Layer']//*[name()='ows:Title'])", doc)
-  const identifier = xpath.select("string(//*[name()='Layer']//*[name()='ows:Identifier'])", doc)
-  const abstract = xpath.select("string(//*[name()='Layer']//*[name()='ows:Abstract'])", doc)
-  const format = xpath.select("string(//*[name()='Layer']//*[name()='Format'])", doc)
+  const title = select('string(//Layer/ows:Title)', doc, true)
+  const identifier = select('string(//Layer/ows:Identifier)', doc, true)
+  const abstract = select('string(//Layer/ows:Abstract)', doc, true)
+  const format = select('string(//Layer/Format)', doc, true)
+  const tileMatrixSets = select('//Layer/TileMatrixSetLink/TileMatrixSet', doc).map(tileMatrixSet => tileMatrixSet.textContent)
   const bbox = selectBBox(doc)
   const zooms = selectZooms(doc)
   return {
@@ -81,7 +110,21 @@ function layer (doc) {
     format: format,
     bbox: bbox,
     minzoom: zooms.minzoom,
-    maxzoom: zooms.maxzoom
+    maxzoom: zooms.maxzoom,
+    tileMatrixSets: tileMatrixSets
+  }
+}
+
+/**
+ * Parse URL
+ *
+ * @param {Document} doc
+ * @returns {URL} url
+ */
+function url (doc) {
+  const host = select("string(//*[name()='ows:ServiceType'])", doc)
+  return {
+    host: host
   }
 }
 
@@ -92,9 +135,9 @@ function layer (doc) {
  * @returns {Service} service
  */
 function service (doc) {
-  const type = xpath.select("string(//*[name()='Capabilities']//*[name()='ows:ServiceType'])", doc)
-  const version = xpath.select("string(//*[name()='Capabilities']//*[name()='ows:ServiceTypeVersion'])", doc)
-  const title = xpath.select("string(//*[name()='Capabilities']//*[name()='ows:Title'])", doc)
+  const type = select("string(//*[name()='ows:ServiceType'])", doc)
+  const version = select("string(//*[name()='ows:ServiceTypeVersion'])", doc)
+  const title = select("string(//*[name()='ows:Title'])", doc)
   return {
     type: type,
     version: version,
@@ -109,9 +152,12 @@ function service (doc) {
  * @returns {Metadata} WMTS Metadata
  */
 module.exports = function (xml) {
+  // Remove xmlns="http://www.opengis.net/wmts/1.0"
+  xml = xml.replace(/xmlns="[\S]+"/, '')
   const doc = new DOMParser().parseFromString(xml)
   return clean({
     service: service(doc),
-    layer: layer(doc)
+    layer: layer(doc),
+    url: url(doc)
   })
 }
